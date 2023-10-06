20dea79425b417aed451bd77826fa7934fbaaefd
buntunji <scia.ng.za@gmail.com>
2023-10-05 22:06:07 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -43,13 +43,14 @@ char **strtow(char *str)
 
 	while (*(str + len))
 		len++;
-	words = count_word(str)
-		if (words == 0)
-			return (NULL);
-	matrix = (char **) malloc(sizeof(char *) * (words + 1))
+	words = count_word(str);
 
-		if (matrix == NULL)
-			return (NULL);
+	if (words == 0)
+		return (NULL);
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+
+	if (matrix == NULL)
+		return (NULL);
 	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
@@ -58,8 +59,10 @@ char **strtow(char *str)
 			{
 				end = i;
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
+
 				if (tmp == NULL)
 					return (NULL);
+
 				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
